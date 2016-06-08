86cc78daecac930aa635b653c78600d6be35ec0b
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-06-07 20:44:05 -0700

    First Assignment, 01_multiples has been added.

---
+++ 01_multiples/part1.py
@@ -0,0 +1,7 @@
+# Part I
+#
+# Create a program that prints all the odd numbers from 1 to 1000.
+# Use the for loop and don't use any array to do this exercise.
+
+for i in range(1, 1000, 2):
+	print i
---
+++ 01_multiples/part2.py
@@ -0,0 +1,6 @@
+# Part II
+#
+# Create another program that prints all the multiples of 5 from 5 to 1,000,000.
+
+for i in range(5, 1000001, 5):
+	print i
