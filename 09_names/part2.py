da887454fd621bb14bdc6361bf01dab2ea1196aa
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-06-09 01:17:48 -0700

    9th Assignment, 09_names has been added.

---
+++ 09_names/part2.py
@@ -0,0 +1,34 @@
+# Part II
+#
+# Now, given the following dictionary:
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+# Create a program that prints  the following format (including number of characters in each combined name):
+#
+# Students
+# 1 - MICHAEL JORDAN - 13
+# 2 - JOHN ROSALES - 11
+# 3 - MARK GUILLEN - 11
+# 4 - KB TONEL - 7
+# Instructors
+# 1 - MICHAEL CHOI - 11
+# 2 - MARTIN PURYEAR - 13
+
+def usersList():
+	for key, data in users.items():
+		print key
+		for name in data:
+			print data.index(name)+1, name['first_name'], name['last_name'], "-", len(name['first_name'])+len(name['last_name'])
+
+usersList()
