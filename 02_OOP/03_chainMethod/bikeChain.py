e18f4fe290e4057f9e8f6d6c013454ef46ce21eb
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-06-13 23:12:16 -0700

    3rd Assignment for OOP: 03_chainMethod has been added. bike.py has been revised: bike3 has been corrected.

--- 02_OOP/03_chainMethod/bikeChain.py
+++ 02_OOP/03_chainMethod/bikeChain.py
@@ -11,35 +11,27 @@ class Bike(object):
 		print "Price: {} dollars".format(self.price)
 		print "Maximum Speed: {} mph".format(self.max_speed)
 		print "Total Miles:", self.miles, "\n"
+		return self
 	def ride(self):
 		print "Riding {}!".format(self.name)
 		self.miles += 10
+		return self
 	def reverse(self):
 		print "Reversing {}!".format(self.name)
 		if (self.miles-5) < 0:
 			self.miles = 0
 		else:
 			self.miles -= 5
+		return self
 
 # bike 1
 bike1 = Bike("bike one", 200, 25)
-bike1.ride()
-bike1.ride()
-bike1.ride()
-bike1.reverse()
-bike1.displayInfo()
+bike1.ride().ride().ride().reverse().displayInfo()
 
 # bike 2
 bike2 = Bike("bike two", 1000, 40)
-bike2.ride()
-bike2.ride()
-bike2.reverse()
-bike2.reverse()
-bike2.displayInfo()
+bike2.ride().ride().reverse().reverse().displayInfo()
 
 # bike 3
 bike3 = Bike("bike three", 800, 35)
-bike3.reverse()
-bike3.reverse()
-bike3.reverse()
-bike3.displayInfo()
+bike3.reverse().reverse().reverse().displayInfo()
