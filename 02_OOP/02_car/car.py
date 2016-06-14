25aff338f5f0701aad336f79ccb956e3cf6f12f2
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-06-13 22:31:05 -0700

    2nd Assignment for OOP: 02_car has been added.

---
+++ 02_OOP/02_car/car.py
@@ -0,0 +1,21 @@
+# car OOP
+
+class Car(object):
+	def __init__(self, price, speed, fuel, mileage):
+		self.price = price
+		self.speed = speed
+		self.fuel = fuel
+		self.mileage = mileage
+		if price > 10000:
+			self.tax = 0.15
+		else:
+			self.tax = 0.12
+		self.display_all()
+	def display_all(self):
+		print "Price: $" + str(self.price)
+		print "Speed: " + str(self.speed) + " mph"
+		print "Fuel: " + str(self.fuel)
+		print "Mileage: " + str(self.mileage) + " mpg"
+		print "Tax: " + str(self.tax)
+
+car1 = Car(10000, 40, 'Full', 15)
