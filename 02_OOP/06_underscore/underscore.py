d1def593b2566783f3dc141567e5ed53fa124415
Nitish Dayal <nitishdayal@outlook.com>
2016-06-27 03:17:26 -0700

    Minor suggestions and recommendations

--- 02_OOP/06_underscore/underscore.py
+++ 02_OOP/06_underscore/underscore.py
@@ -1,57 +1,69 @@
 # underscore
 class Underscore(object):
-	def map(self, arr, callback):
-		arrNew = []
-		for data in arr:
-			arrNew.append(callback(data))
-		return arrNew
-	def reduce(self, arr, callback, initial = None):
-		it = iter(arr)
-		if initial is None:
-			try:
-				initial = next(it)
-			except StopIteration:
-				raise TypeError('reduce() of empty sequence with no initial value')
-		reduced = initial
-		for data in it: # starts from idx of 0 or 1 depends on initial was None or not.
-			reduced = callback(reduced, data)
-		return reduced
-	def find(self, arr, callback):
-		for data in arr:
-			if callback(data) == True:
-				return data
-		return None
-	def filter(self, arr, callback):
-		arrNew = []
-		for data in arr:
-			if callback(data) == True:
-				arrNew.append(data)
-		return arrNew
-	def reject(self, arr, callback):
-		arrNew = []
-		for data in arr:
-			if callback(data) == False:
-				arrNew.append(data)
-		return arrNew
 
-testList = [1,2,3,4,5,6] # this should be untouched
+    def map(self, arr, callback):
+        arrNew = []
+        for data in arr:
+            arrNew.append(callback(data))
+        return arrNew
+
+    def reduce(self, arr, callback, initial=None):
+        it = iter(arr)
+        if initial is None:
+            try:
+                initial = next(it)
+            except StopIteration:
+                raise TypeError(
+                    'reduce() of empty sequence with no initial value')
+        reduced = initial
+        for data in it:
+            # starts from idx of 0 or 1 depends on initial was None or not.
+            reduced = callback(reduced, data)
+        return reduced
+
+    """
+    Easier way to check if value exists or not is just to say
+    'if variable:' or 'if not variable:'
+    """
+
+    def find(self, arr, callback):
+        for data in arr:
+            if callback(data):
+                return data
+        return None
+
+    def filter(self, arr, callback):
+        arrNew = []
+        for data in arr:
+            if callback(data):
+                arrNew.append(data)
+        return arrNew
+
+    def reject(self, arr, callback):
+        arrNew = []
+        for data in arr:
+            if not callback(data):
+                arrNew.append(data)
+        return arrNew
+
+testList = [1, 2, 3, 4, 5, 6]  # this should be untouched
 _ = Underscore()
 
-# evens = _.filter(testList, lambda x: x % 2 == 0)
-# print testList
-# print evens
+evens = _.filter(testList, lambda x: x % 2 == 0)
+print testList
+print evens
 
-# addTwo = _.map(testList, lambda x: x+2)
-# print testList
-# print addTwo
+addTwo = _.map(testList, lambda x: x + 2)
+print testList
+print addTwo
 
-# sum = _.reduce(testList, lambda x, y: x+y, 2)
-# print testList
-# print sum
+sum = _.reduce(testList, lambda x, y: x + y, 2)
+print testList
+print sum
 
-# firstEven = _.find(testList, lambda x: x % 2 == 0)
-# print testList
-# print firstEven
+firstEven = _.find(testList, lambda x: x % 2 == 0)
+print testList
+print firstEven
 
 notEvens = _.reject(testList, lambda x: x % 2 == 0)
 print testList
--- 03_Flask/01_landingPage/landingPage.py
+++ 03_Flask/01_landingPage/landingPage.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+"""
+This one is up to you: I add an additonal '/' after
+my routes. This allows the user to navigate to a URL
+which either has a trailing backslach or not and still be
+taken to the correct page. Without the trailing backslash
+included in the route decorator, a user would receive a 404
+if they attempted to navigate to '/ninja/' instead of '/ninja'.
+"""
+
+
 @app.route('/')
 def index():
-	return render_template('index.html')
-@app.route('/ninjas')
+    return render_template('index.html')
+
+
+@app.route('/ninjas/')
 def ninjas():
-	return render_template('ninjas.html')
-@app.route('/dojos/new')
+    return render_template('ninjas.html')
+
+
+@app.route('/dojos/new/')
 def dojos():
-	return render_template('dojos.html')
+    return render_template('dojos.html')
 
 app.run(debug=True)
